50d757bb2703a942ababc9ed6c924b41dc37d7f3
ARthi ANeel <arthianeel2000@gmail.com>
2022-04-05 22:29:08 +0530

    webpack

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+//Create App Component
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
